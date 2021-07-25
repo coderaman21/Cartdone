919b8e975c932bac6520ce5fcfcfcecdb3595cc9
aman <amans197555@gmail.com>
2021-07-25 16:44:58 +0530

    updated settings.py again

--- shopee/settings.py
+++ shopee/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-import os
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
