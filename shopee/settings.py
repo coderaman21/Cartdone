74256b0767266ba67ea1b1f5b804c3a07817d16b
aman <amans197555@gmail.com>
2021-07-25 16:42:34 +0530

    updated settings.py

--- shopee/settings.py
+++ shopee/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # managing media 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -139,3 +141,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
