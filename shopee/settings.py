69f1553a0799da93079bec0f2d4a298d8e127a48
Divyanshu soni <amans197555@gmail.com>
2023-02-02 12:00:38 +0530

    vercel deployment

--- shopee/settings.py
+++ shopee/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o--h!10)ov#_tthy4!82m)z8zo)(m6*g^l&-0aafb^1$&nb-(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://cartdone.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '.now.sh']
 
 
 MESSAGE_TAGS = {
@@ -135,7 +135,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # managing media 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
