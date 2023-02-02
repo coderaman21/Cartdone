be913cb94857bbc5039ccfebdf6e480ca43bb256
Divyanshu soni <amans197555@gmail.com>
2023-02-02 12:08:28 +0530

    bug fix for deployment

--- shopee/settings.py
+++ shopee/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # managing media 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
